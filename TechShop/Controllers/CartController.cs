c963955e202b846056b778ff7f5f802a5d60ce1d
dhlg1995 <dhlg1995@gmail.com>
2023-08-28 21:16:35 +0700

    cart

---
+++ TechShop/Controllers/CartController.cs
@@ -0,0 +1,59 @@
+﻿using Infrastructure.Products;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.IdentityModel.Tokens.Jwt;
+using TechShop.Models;
+
+namespace TechShop.Controllers
+{
+    public class CartController:Controller
+    {
+        private readonly ILogger<CartController> _logger;
+        private readonly IProductService _productService;
+        public CartController(ILogger<CartController> logger,IProductService productService)
+        {
+
+            _logger = logger;
+            _productService = productService;
+
+        }
+
+        public IActionResult AddToCart(ProductViewModel product)
+        {
+            var rs = "";
+            CartViewModel cart ;
+            if (product == null) rs = "Product is not found!";
+            else if (product != null)
+            {
+                cart = new CartViewModel()
+                {
+                    ProductId = product.Id,
+                    ImageLink = product.Image,
+                    Price = product.Price,
+                    Quantity= product.Quantity
+
+                };
+            }
+
+
+            return Json();
+        }
+
+    }
+
+
+    public static class CartExtension
+    {
+        public static string _key = "cart";
+        public static void setCart(this ISession session,string key,List<CartViewModel> lscart)
+        {
+            session.SetString(key, JsonExtensions.SerializeToJson(lscart));
+        }
+        public static List<CartViewModel> getCart(this ISession session,string key)
+        {
+            var rs = JsonExtensions.DeserializeFromJson<List<CartViewModel>>(key);
+            return rs;
+        }
+    }
+}
---
+++ TechShop/Models/CartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace TechShop.Models
+{
+    public class CartViewModel
+    {
+        public Guid ProductId { get; set; }
+        public string ImageLink { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
